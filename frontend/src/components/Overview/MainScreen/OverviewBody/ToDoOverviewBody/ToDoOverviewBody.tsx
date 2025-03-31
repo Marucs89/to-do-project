e49f94fcaf75c41f99c2a9e7fc9660a35d6aa510
Whatdoyoumean04 <finnbecher@t-online.de>
2025-03-31 10:12:12 +0200

    test

---
+++ frontend/src/components/Overview/MainScreen/OverviewBody/ToDoOverviewBody/ToDoOverviewBody.tsx
@@ -0,0 +1,24 @@
+import "./ToDoOverviewBody.css"
+
+export default function ToDoOverviewBody() {
+  return (
+    <div>
+      <h2>Subject</h2>
+      <button>+</button>
+      <div className="status-frame">
+        <h3>Description: Kündigung von Herrn Ammann</h3>
+        <h4>Due Date: 10.03.2025</h4>
+        <h5>Bearbeiter: Macel Barth</h5>  
+        <h6>Status: In Bearbeitung</h6>
+      </div>
+      <div className="Task2">
+        <h3>Description: Desing der Website erstellen</h3>
+        <h4>Due Date: 10.03.2025</h4>
+        <h5>Bearbeiter: Finn Becher</h5>  
+        <h6>Status: In Bearbeitung</h6>
+      </div>
+    </div>
+  );
+ 
+}
+
